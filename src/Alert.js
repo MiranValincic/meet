2a9346aaf0b65d128d1ce3b828dc590fe63dcc60
MiranValincic <80262902+MiranValincic@users.noreply.github.com>
2022-04-25 20:36:26 +0200

    Added welcome srcreen and privacy policy

--- src/Alert.js
+++ src/Alert.js
@@ -37,5 +37,13 @@ class ErrorAlert extends Alert {
   }
 }
 
+class OfflineAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "yellow";
+  }
+}
+
 export { InfoAlert };
 export { ErrorAlert };
+export { OfflineAlert };
